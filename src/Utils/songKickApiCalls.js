9a0e7b1ac8f02f8ca732d261e7aa0a6e672baede
Gray Smith <gray.smith00@gmail.com>
2018-10-25 16:31:37 -0600

    Add getArtistInfo api calls

--- src/Utils/songKickApiCalls.js
+++ src/Utils/songKickApiCalls.js
@@ -10,11 +10,54 @@ export const getMatchingVenue = async (venueName, city) => {
   }
   const result = data.resultsPage.results.venue[0];
   const venue = {
+    street_address: result.street,
     city: result.city.displayName,
     state: result.city.state.displayName,
     name: result.displayName,
-    zip: result.zip,
-    capacity: result.capacity
+    zip: parseInt(result.zip),
+    capacity: result.capacity,
+    venue_songkick_id: result.id
   };
   return venue;
+};
+
+export const getArtistInfo = async (artistName, agency) => {
+  const url = `https://api.songkick.com/api/3.0/search/artists.json?apikey=${
+    process.env.REACT_APP_SONGKICK_KEY
+  }&query=${artistName}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const spotifyInfo = await getSpotifyInfo(artistName);
+
+  return {
+    name: artistName,
+    agency: agency,
+    songkick_id: data.resultsPage.results.artist[0].id,
+    ...spotifyInfo
+  };
+};
+
+export const getSpotifyInfo = async artistName => {
+  const url = 'https://api.spotify.com/v1/search?q=justice&type=artist';
+  const response = await fetch(url, {
+    headers: {
+      Authorization: 'Bearer ' + process.env.REACT_APP_SPOTIFY_BEARER
+    }
+  });
+  const data = await response.json();
+  const result = data.artists.items[0];
+
+  const spotifyInfo = {
+    name: result.name,
+    image_url: result.images[0].url,
+    popularity: result.popularity,
+    spotify_followers: result.followers.total,
+    spotify_url: result.external_urls.spotify,
+    spotify_id: result.id,
+    genres: result.genres.join(', ')
+  };
+
+  return spotifyInfo;
 };
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import Routes from '../Routes/Routes';
+import { getArtistInfo } from '../../Utils/songKickApiCalls';
 import './App.css';
 
 class App extends Component {
+  async componentDidMount() {
+    const artistInfo = await getArtistInfo('justice', 'WME');
+    console.log(artistInfo);
+  }
+
   render() {
     return (
       <div className="app">
