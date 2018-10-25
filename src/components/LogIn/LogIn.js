f5c73cbcc1fee651b365c564f5865dacd74d3de9
Gray Smith <gray.smith00@gmail.com>
2018-10-24 18:06:52 -0600

    User Login request working

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,33 +1,60 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { userLogIn } from '../../Utils/backendApiCalls';
 import './LogIn.css';
 
 class LogIn extends Component {
   state = {
-    email: "",
-    password: ""
-  }
+    email: '',
+    password: ''
+  };
 
-  handleChange = (e) => {
+  handleChange = e => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
-  }
+  };
+
+  handleSubmit = async e => {
+    const { email, password } = this.state;
+    e.preventDefault();
+    const userCreds = {
+      buyer: {
+        email,
+        password
+      }
+    };
+
+    const savedUser = await userLogIn(userCreds);
+  };
 
   render() {
     const { closeLogInModal } = this.props;
     return (
-      <form className="log-in">
+      <form className="log-in" onSubmit={this.handleSubmit}>
         <div className="inner-modal">
           <h1>Log In</h1>
           <div className="input-wrapper">
             <h4>Email:</h4>
-            <input type="email" name="email" value={this.state.email} placeholder="email" onChange={this.handleChange} />
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              placeholder="email"
+              onChange={this.handleChange}
+            />
           </div>
           <div className="input-wrapper">
             <h4>Password:</h4>
-            <input type="text" name="password" value={this.state.password} placeholder="password" onChange={this.handleChange} />
+            <input
+              type="text"
+              name="password"
+              value={this.state.password}
+              placeholder="password"
+              onChange={this.handleChange}
+            />
           </div>
           <p onClick={closeLogInModal}>x</p>
+          <button>Submit</button>
         </div>
       </form>
     );
