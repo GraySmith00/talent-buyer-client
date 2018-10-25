23440b73f15186cddb65b1165d7f97eacc42c80c
Gray Smith <gray.smith00@gmail.com>
2018-10-24 18:06:40 -0600

    Add userLogIn backend api call method

--- src/Utils/backendApiCalls.js
+++ src/Utils/backendApiCalls.js
@@ -14,3 +14,22 @@ export const userSignUp = async user => {
   const data = await response.json();
   return data;
 };
+
+export const userLogIn = async userCreds => {
+  const url = `http://localhost:5000/login`;
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(userCreds),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.status === 201) {
+    const data = await response.json();
+    return data;
+  } else {
+    const error = await response.text();
+    console.log(error);
+  }
+};
