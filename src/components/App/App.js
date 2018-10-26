f0f7c9d6f5aedb19d5ef26edc34739c99a9c0ed7
Gray Smith <gray.smith00@gmail.com>
2018-10-25 21:02:58 -0600

    Remove unused method calls in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react';
 import Routes from '../Routes/Routes';
-import { getArtistInfo, getSpotifyInfo } from '../../Utils/songKickApiCalls';
-import {
-  artistPostRequest,
-  addWmeArtistsToBackend,
-  addCaaArtistsToBackend
-} from '../../Utils/backendApiCalls';
 import './App.css';
 
 class App extends Component {
-  async componentDidMount() {
-    // const artistInfo = await getArtistInfo('justice', 'WME');
-    // console.log(artistInfo);
-    // await addCaaArtistsToBackend();
-  }
-
   render() {
     return (
       <div className="app">
