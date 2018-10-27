5942800b298db22bdbd4ac210550827ca369dbca
Gray Smith <gray.smith00@gmail.com>
2018-10-27 16:09:34 -0600

    Add react-calendar to Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import Calendar from 'react-calendar';
 import Nav from '../Nav/Nav';
 import './Dashboard.css';
 
 class Dashboard extends Component {
+  state = {
+    date: new Date()
+  };
+
+  handleDateChange = date => this.setState({ date });
+
   render() {
     return (
       <div className="dashboard">
         <Nav />
-        <h1>Dashboard</h1>
+        <Calendar onChange={this.handleDateChange} value={this.state.date} />
       </div>
     );
   }
