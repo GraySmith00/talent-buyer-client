f587d0ce5dfd453fc26c90fb390693615be6b2eb
Gray Smith <gray.smith00@gmail.com>
2018-10-27 15:53:23 -0600

    Add basic styling for Dashbaord and Nav

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Nav from '../Nav/Nav';
+import './Dashboard.css';
 
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="dashboard">
         <Nav />
+        <h1>Dashboard</h1>
       </div>
     );
   }
