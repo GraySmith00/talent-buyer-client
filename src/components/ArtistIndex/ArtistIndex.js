0dc4ee51d4e02083bcf0975c2a661f0ece9f1c9d
Gray Smith <gray.smith00@gmail.com>
2018-10-28 12:02:02 -0600

    Get all artists working on ArtistIndex component

--- src/components/ArtistIndex/ArtistIndex.js
+++ src/components/ArtistIndex/ArtistIndex.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
+import { getAllArtists } from '../../Utils/backendApiCalls';
+import axios from 'axios';
 import Nav from '../Nav/Nav';
 
+import './ArtistIndex.css';
+
 class ArtistIndex extends Component {
+  state = {
+    artists: []
+  };
+
+  async componentDidMount() {
+    axios.defaults.headers.common['Authorization'] = localStorage.getItem(
+      'jwtToken'
+    );
+    const artists = await getAllArtists();
+    this.setState({ artists });
+  }
+
   render() {
+    const { artists } = this.state;
+    let displayArtists;
+
+    if (!artists.length) {
+      displayArtists = <p>Loading...</p>;
+    } else {
+      displayArtists = artists.slice(0, 25).map(artist => (
+        <div key={artist.id} className="artist-listing">
+          <img src={artist.image_url} alt="artist" className="artist-image" />
+          <p>{artist.name}</p>
+          <p>{artist.agency}</p>
+          <p>{artist.popularity}</p>
+          <p>{artist.spotify_followers}</p>
+        </div>
+      ));
+    }
+
     return (
-      <div>
-        <h1>Artist Index</h1>
+      <div className="artist-index">
         <Nav />
+        <div className="main-content">
+          <div className="table-headings">
+            <h3>Image</h3>
+            <h3>Name</h3>
+            <h3>Agency</h3>
+            <h3>Popularity</h3>
+            <h3>Spotify Followers</h3>
+          </div>
+          <div className="artists-container">{displayArtists}</div>
+        </div>
       </div>
     );
   }
