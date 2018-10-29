cdfcd8dcb4c73c99d97cea7bd569a6af83cd4ff6
Gray Smith <gray.smith00@gmail.com>
2018-10-28 21:14:33 -0600

    Fix tests, add mockData, fix linter errors

---
+++ src/Utils/__mocks__/backendApiCalls.js
@@ -0,0 +1,23 @@
+import { mockArtists } from '../mockData';
+
+export const signUpPostRequest = jest.fn();
+
+export const logInPostRequest = jest.fn();
+
+export const venuePostRequest = jest.fn();
+
+export const getAllUserVunues = jest.fn();
+
+export const artistPostRequest = jest.fn();
+
+export const addWmeArtistsToBackend = jest.fn();
+
+export const addCaaArtistsToBackend = jest.fn();
+
+export const getAllArtists = jest.fn().mockImplementation(() => {
+  return Promise.resolve(mockArtists);
+});
+
+export const offerPostRequest = jest.fn();
+
+export const getAllUserOffers = jest.fn();
--- src/Utils/mockData.js
+++ src/Utils/mockData.js
@@ -10,3 +10,192 @@ export const mockArtist = {
   genres:
     'alternative dance, dance-punk, electro house, electronic, filter house, indietronica, new rave'
 };
+
+export const mockArtists = [
+  {
+    id: 4,
+    name: 'Alesso',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/021f014a6809e5b7f1ef1c18bf247fd9ccacb193',
+    popularity: 81,
+    songkick_id: 4329851,
+    spotify_url: 'https://open.spotify.com/artist/4AVFqumd2ogHFlRbKIjp1t',
+    spotify_id: '4AVFqumd2ogHFlRbKIjp1t',
+    spotify_followers: 2296228
+  },
+  {
+    id: 5,
+    name: 'Adam Beyer',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/1030d6e68eb67ff57d4ee04121d3450ec35e9d14',
+    popularity: 54,
+    songkick_id: 173218,
+    spotify_url: 'https://open.spotify.com/artist/1btv9qmIpbp7q1ixCYNdHu',
+    spotify_id: '1btv9qmIpbp7q1ixCYNdHu',
+    spotify_followers: 128307
+  },
+  {
+    id: 6,
+    name: 'AlunaGeorge',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/2cb91f6664f67d9bce335abd4bbf4a73a7ed9af3',
+    popularity: 71,
+    songkick_id: 3909671,
+    spotify_url: 'https://open.spotify.com/artist/2VAnyOxzJuSAj7XIuEOT38',
+    spotify_id: '2VAnyOxzJuSAj7XIuEOT38',
+    spotify_followers: 326986
+  },
+  {
+    id: 7,
+    name: 'Afrojack',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/bec25756309b1f7af565252f78709d69a8f11974',
+    popularity: 75,
+    songkick_id: 2479031,
+    spotify_url: 'https://open.spotify.com/artist/4D75GcNG95ebPtNvoNVXhz',
+    spotify_id: '4D75GcNG95ebPtNvoNVXhz',
+    spotify_followers: 2309822
+  },
+  {
+    id: 8,
+    name: 'Apster',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/97ac608af943e1f01488aa38da85882cb58e7029',
+    popularity: 40,
+    songkick_id: 3494066,
+    spotify_url: 'https://open.spotify.com/artist/0bi8sypegPFiP6Rd9gdkyz',
+    spotify_id: '0bi8sypegPFiP6Rd9gdkyz',
+    spotify_followers: 4236
+  },
+  {
+    id: 9,
+    name: 'Annie Lennox',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/fb1b39783fdc9625bcba0837e7da9882969eaa03',
+    popularity: 63,
+    songkick_id: 2476291,
+    spotify_url: 'https://open.spotify.com/artist/5MspMQqdVbdwP6ax3GXqum',
+    spotify_id: '5MspMQqdVbdwP6ax3GXqum',
+    spotify_followers: 437122
+  },
+  {
+    id: 10,
+    name: 'Bag Raiders',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/da28b5d29dc75072d83b5892ea3db97926cbe914',
+    popularity: 60,
+    songkick_id: 975813,
+    spotify_url: 'https://open.spotify.com/artist/6fXEqmGQEt6ONuqVmwrN46',
+    spotify_id: '6fXEqmGQEt6ONuqVmwrN46',
+    spotify_followers: 136557
+  },
+  {
+    id: 11,
+    name: 'Basement Jaxx',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/4c74994e83cacb4ee9b5fce6d6dcbe937b8642e6',
+    popularity: 59,
+    songkick_id: 44913,
+    spotify_url: 'https://open.spotify.com/artist/4YrKBkKSVeqDamzBPWVnSJ',
+    spotify_id: '4YrKBkKSVeqDamzBPWVnSJ',
+    spotify_followers: 249350
+  },
+  {
+    id: 12,
+    name: 'BAYNK',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/6405fd617f1f99516c6577dd707e34784377683a',
+    popularity: 60,
+    songkick_id: 8685484,
+    spotify_url: 'https://open.spotify.com/artist/28yVvEvA2lT3K5RNIhV1Dj',
+    spotify_id: '28yVvEvA2lT3K5RNIhV1Dj',
+    spotify_followers: 26214
+  },
+  {
+    id: 13,
+    name: 'Ben Nicky',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/fa65f6fcd9924e85222268af9ececbf92e88d4cd',
+    popularity: 45,
+    songkick_id: 1403293,
+    spotify_url: 'https://open.spotify.com/artist/3Bd1phrOZJuCev9U0bzdtA',
+    spotify_id: '3Bd1phrOZJuCev9U0bzdtA',
+    spotify_followers: 18730
+  },
+  {
+    id: 14,
+    name: 'Björk',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/5001ca3c3cd95734aac7320d2199b9b515595375',
+    popularity: 63,
+    songkick_id: 492728,
+    spotify_url: 'https://open.spotify.com/artist/7w29UYBi0qsHi5RTcv3lmA',
+    spotify_id: '7w29UYBi0qsHi5RTcv3lmA',
+    spotify_followers: 803225
+  },
+  {
+    id: 15,
+    name: 'Black Coffee',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/db715976679d76f7c79c54c0f97c72c5637ed155',
+    popularity: 65,
+    songkick_id: 421804,
+    spotify_url: 'https://open.spotify.com/artist/6wMr4zKPrrR0UVz08WtUWc',
+    spotify_id: '6wMr4zKPrrR0UVz08WtUWc',
+    spotify_followers: 155714
+  },
+  {
+    id: 18,
+    name: 'Booka Shade',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/8c0fbe0fb1c86825adeb18bf0a019a22827c4422',
+    popularity: 51,
+    songkick_id: 295932,
+    spotify_url: 'https://open.spotify.com/artist/2CKaDZ1Yo8YnWega9IeUzB',
+    spotify_id: '2CKaDZ1Yo8YnWega9IeUzB',
+    spotify_followers: 113010
+  },
+  {
+    id: 19,
+    name: 'Calvin Harris',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/7b4c05bee8c822c006b372e26ef1b08acc3af45a',
+    popularity: 90,
+    songkick_id: 82547,
+    spotify_url: 'https://open.spotify.com/artist/7CajNmpbOovFoOoasH2HaY',
+    spotify_id: '7CajNmpbOovFoOoasH2HaY',
+    spotify_followers: 13691351
+  },
+  {
+    id: 22,
+    name: 'Chris Lake',
+    agency: 'WME',
+    image_url:
+      'https://i.scdn.co/image/b5a5ac37867b209a77e5448152b2861e1ff0a333',
+    popularity: 65,
+    songkick_id: 211161,
+    spotify_url: 'https://open.spotify.com/artist/5Igpc9iLZ3YGtKeYfSrrOE',
+    spotify_id: '5Igpc9iLZ3YGtKeYfSrrOE',
+    spotify_followers: 77561
+  }
+];
+
+export const mockHistory = {
+  length: 50,
+  action: 'PUSH',
+  location: { pathname: '/home', search: '', hash: '', key: 'hvocjm' }
+};
