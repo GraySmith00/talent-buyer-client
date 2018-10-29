ee796f8f0628a3d0cd4e365d28a6e7bf05620eb9
Gray Smith <gray.smith00@gmail.com>
2018-10-28 18:46:18 -0600

    All user offers populating in redux store on login. Add new offer working in redux store.

---
+++ src/actions/offerActions.js
@@ -0,0 +1,33 @@
+import { getAllUserOffers, offerPostRequest } from '../Utils/backendApiCalls';
+
+// set user offers action
+export const setOffersState = offers => ({
+  type: 'SET_OFFERS_STATE',
+  offers
+});
+
+// populate user offers thunk
+export const populateUserOffers = userId => async dispatch => {
+  const userOffers = await getAllUserOffers(userId);
+  if (!userOffers.length) {
+    return [];
+  }
+  dispatch(setOffersState(userOffers));
+  return userOffers;
+};
+
+// add new offer action
+export const addNewOfferToState = offer => ({
+  type: 'ADD_NEW_OFFER_TO_STATE',
+  offer
+});
+
+// add new offer thunk
+export const addNewOffer = offer => async dispatch => {
+  const savedOffer = await offerPostRequest(offer);
+  if (!savedOffer) {
+    throw new Error({ message: 'Something went wrong' });
+  }
+  dispatch(addNewOfferToState(savedOffer));
+  return savedOffer;
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { userReducer } from './userReducer';
 import { venueReducer } from './venueReducer';
+import { offerReducer } from './offerReducer';
 
 const rootReducer = combineReducers({
   currentUser: userReducer,
-  currentVenue: venueReducer
+  currentVenue: venueReducer,
+  offers: offerReducer
 });
 
 export default rootReducer;
