8201a73e9c59e93ee5462f396904f5eea673c76d
Gray Smith <gray.smith00@gmail.com>
2018-10-28 21:25:07 -0600

    Fix Dashboard merge conflicts

---
+++ src/components/OffersContainer/OffersContainer.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import './OffersContainer.css';
+
+class OffersContainer extends Component {
+  formatDate = date => {
+    let day = date.getDate();
+    if (day < 10) day = `0${day}`;
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${year}-${month}-${day}`;
+  };
+
+  render() {
+    const { offers, date } = this.props;
+    const formattedDate = this.formatDate(date);
+
+    const filteredOffers = offers.filter(offer => offer.date === formattedDate);
+
+    let displayOffers;
+
+    if (filteredOffers.length) {
+      displayOffers = filteredOffers.map(offer => (
+        <div className="table-row" key={offer.id}>
+          <h3>{offer.artist_name}</h3>
+          <h3>{offer.status}</h3>
+          <button className="view-offer-button">View Offer</button>
+        </div>
+      ));
+    }
+
+    return <div className="offers-container">{displayOffers}</div>;
+  }
+}
+
+OffersContainer.propTypes = {
+  offers: PropTypes.array.isRequired,
+  date: PropTypes.object.isRequired
+};
+
+export const mapStateToProps = state => ({
+  offers: state.offers
+});
+
+export default connect(mapStateToProps)(OffersContainer);
--- src/reducers/offerReducer.js
+++ src/reducers/offerReducer.js
@@ -2,10 +2,8 @@ export const offerReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_OFFERS_STATE':
       return action.offers;
-    case 'ADD_NEW_OFFER_TO_STATE': {
-      const newOffer = action.offer;
+    case 'ADD_NEW_OFFER_TO_STATE':
       return [...state, action.offer];
-    }
     default:
       return state;
   }
