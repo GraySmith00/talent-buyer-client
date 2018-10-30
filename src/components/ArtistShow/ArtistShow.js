78040be6b296ee7a9c7a58c2ac74152beb627c27
Tim Fischer <emailtimfischer@gmail.com>
2018-10-29 19:11:13 -0600

    Save artist name and image url in local state

--- src/components/ArtistShow/ArtistShow.js
+++ src/components/ArtistShow/ArtistShow.js
@@ -4,18 +4,25 @@ import './ArtistShow.css';
 import { getArtist } from '../../Utils/backendApiCalls';
 
 class ArtistShow extends Component {
+  state = {
+    name: "",
+    image_url: ""
+  }
 
   async componentDidMount() {
     const { id } = this.props;
     const artist = await getArtist(id);
+    this.setState({ name: artist.name, image_url: artist.image_url });
   }
 
   render() {
+    const { name, image_url } = this.state;
     return (
       <div className="artist-show">
         <Nav />
         <div className="main-content">
-          <h1>Artist Show</h1>
+          <h1>{name}</h1>
+          <img className="profile-image" src={image_url} alt="artist-display-image" />
         </div>
       </div>
     );
