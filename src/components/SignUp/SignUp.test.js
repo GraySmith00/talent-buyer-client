344149c30a1b1d961296db4e1c5f0a3d520a9ba9
Gray Smith <gray.smith00@gmail.com>
2018-10-31 20:38:33 -0600

    Work on SignUp test

--- src/components/SignUp/SignUp.test.js
+++ src/components/SignUp/SignUp.test.js
@@ -1,26 +1,68 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { mockSignUpState } from '../../Utils/mockData';
+import { mockHistory } from '../../Utils/mockData';
 import { setNewVenue } from '../../actions/venueActions';
+import { registerUser } from '../../actions/userActions';
 
-import { SignUp, saveVenue } from './SignUp';
+import { SignUp } from './SignUp';
+
+jest.mock('../../actions/venueActions');
+jest.mock('../../actions/userActions');
 
 describe('SignUp component', () => {
   let wrapper;
   let mockCloseSignUpModal;
+  let mockRegisterUser;
 
   beforeEach(() => {
     mockCloseSignUpModal = jest.fn();
-    wrapper = shallow(<SignUp closeSignUpModal={mockCloseSignUpModal} />);
+    mockRegisterUser = jest.fn();
+
+    wrapper = shallow(
+      <SignUp
+        closeSignUpModal={mockCloseSignUpModal}
+        history={mockHistory}
+        registerUser={mockRegisterUser}
+      />
+    );
   });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('saveVenue, should save the current venue', async () => {
-    await wrapper.setState({ mockSignUpState });
-    await wrapper.instance().saveVenue();
-    await expect(setNewVenue).toHaveBeenCalled();
+  it('should have an initial state', () => {
+    const expected = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      venueName: '',
+      venueCity: '',
+      venueError: '',
+      userError: ''
+    };
+
+    expect(wrapper.state()).toEqual(expected);
+  });
+
+  describe('handleChange', () => {
+    it.skip('should change state when an input is changed', () => {
+      wrapper.find('input').simulate('change', { target: { value: 'Bob' } });
+    });
+  });
+
+  describe('saveVenue', () => {
+    it.skip('should call setNewVenue', () => {
+      wrapper.instance().saveVenue();
+      expect(setNewVenue).toHaveBeenCalled();
+    });
+  });
+
+  describe('saveUser', () => {
+    it.skip('should call registerUser', () => {
+      wrapper.instance().saveUser();
+      expect(registerUser).toHaveBeenCalled();
+    });
   });
 });
