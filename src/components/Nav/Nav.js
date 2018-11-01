ad231bc9a64ff38b848a34c3d0742054df570896
Gray Smith <gray.smith00@gmail.com>
2018-10-31 22:43:05 -0600

    Refactor NavLink and modify styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import NavLink from '../NavLink/NavLink';
+import { NavLink } from 'react-router-dom';
 import './Nav.css';
 
 class Nav extends Component {
@@ -7,12 +7,21 @@ class Nav extends Component {
     return (
       <div className="nav">
         <div className="logo-wrap">
-          <img className="frequency" src={require('./frequency.svg')} alt="frequency-logo" />
+          <img
+            className="frequency"
+            src={require('./frequency.svg')}
+            alt="frequency-logo"
+          />
           <h1 className="logo">TalentBC</h1>
         </div>
         <section className="nav-link-wrapper">
-          <NavLink displayText="Home" routeLink="/home" />
-          <NavLink displayText="Artists" routeLink="/artists" />
+          <NavLink to="/home" className="nav-link">
+            Home
+          </NavLink>
+
+          <NavLink to="/artists" className="nav-link">
+            Artists
+          </NavLink>
         </section>
       </div>
     );
