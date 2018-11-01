71b80fb21314a6dd9ff7ec81399c2779835f2c7b
Gray Smith <gray.smith00@gmail.com>
2018-10-31 23:27:02 -0600

    Add venue info to Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './Nav.css';
 
-class Nav extends Component {
+export class Nav extends Component {
   render() {
+    const { currentVenue } = this.props;
     return (
       <div className="nav">
         <div className="logo-wrap">
@@ -18,14 +21,32 @@ class Nav extends Component {
           <NavLink to="/home" className="nav-link">
             Home
           </NavLink>
-
           <NavLink to="/artists" className="nav-link">
             Artists
           </NavLink>
+        </section>
+        <section className="venue-info">
+          <header>Current Venue</header>
+          <div className="venue-info-main">
+            <p>{currentVenue.name}</p>
+            <p>{currentVenue.street_address}</p>
+            <p>
+              {currentVenue.city}, {currentVenue.state}
+            </p>
+            <p>Capacity: {currentVenue.capacity}</p>
+          </div>
         </section>
       </div>
     );
   }
 }
 
-export default Nav;
+Nav.propTypes = {
+  currentVenue: PropTypes.object.isRequired
+};
+
+export const mapStateToProps = state => ({
+  currentVenue: state.currentVenue
+});
+
+export default connect(mapStateToProps)(Nav);
