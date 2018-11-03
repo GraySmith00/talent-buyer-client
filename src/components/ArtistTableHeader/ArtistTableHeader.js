cfe704dfdd53c4bba1d1de4c52cc74535d34d8aa
Tim Fischer <emailtimfischer@gmail.com>
2018-11-02 22:22:04 -0600

    Create ArtistTableHeader component, add classnames npm to allow sort

---
+++ src/components/ArtistTableHeader/ArtistTableHeader.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './ArtistTableHeader.css';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+
+const ArtistTableHeader = ({ category, onClick, activeSort, name }) => {
+  const buttonClass = classNames({
+    header: true,
+    active: activeSort === name
+  });
+  return (
+    <h3
+      className={buttonClass}
+      name={name}
+      onClick={onClick}
+      style={{ cursor: 'pointer' }}
+    >
+      {category}
+    </h3>
+  );
+};
+
+ArtistTableHeader.propTypes = {
+  category: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  activeSort: PropTypes.string,
+  name: PropTypes.string.isRequired
+};
+
+export default ArtistTableHeader;
