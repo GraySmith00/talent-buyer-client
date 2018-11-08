b87a152ed1ba9ba7e5e601481afb5a94c2243094
Gray Smith <gray.smith00@gmail.com>
2018-11-08 12:55:22 -0700

    Style ViewOfferModal

--- src/components/ViewOfferModal/ViewOfferModal.js
+++ src/components/ViewOfferModal/ViewOfferModal.js
@@ -20,7 +20,7 @@ export class ViewOfferModal extends Component {
   }
 
   render() {
-    const { closeViewOfferModal, currentVenue } = this.props;
+    const { closeViewOfferModal, currentVenue, currentUser } = this.props;
     const { offer } = this.state;
 
     let displayOfferInfo;
@@ -30,17 +30,85 @@ export class ViewOfferModal extends Component {
     } else {
       displayOfferInfo = (
         <div className="offer-info">
-          <p>Artist: {offer.artist_name}</p>
-          <p>Guarentee: ${offer.guarantee}</p>
-          <p>Venue: {currentVenue.name}</p>
+          <div className="view-offer-top">
+            <section className="basic-details">
+              <h3>Offer</h3>
+              <p>
+                <span className="field">Date:</span> {offer.date}
+              </p>
+              <p>
+                <span className="field">spanArtist:</span> {offer.artist_name}
+              </p>
+              <p>
+                <span className="field">Buyer:</span> {currentUser.first_name}{' '}
+                {currentUser.last_name}
+              </p>
+              <p>
+                <span className="field">Buyer Email:</span> {currentUser.email}
+              </p>
+            </section>
+            <section className="view-venue-info">
+              <h3>Venue Info</h3>
+              <p>
+                <span className="field">Venue:</span> {currentVenue.name}
+              </p>
+              <p>
+                <span className="field">Address:</span>{' '}
+                {currentVenue.street_address}
+              </p>
+              <p>
+                <span className="field">City:</span> {currentVenue.city},{' '}
+                {currentVenue.state}
+              </p>
+              <p>
+                <span className="field">Capacity:</span> {currentVenue.capacity}
+              </p>
+            </section>
+          </div>
+          <div className="view-offer-bottom">
+            <section className="terms">
+              <h3>Terms</h3>
+              <p>
+                <span className="field">Bonuses:</span> ${offer.bonuses}
+              </p>
+              <p>
+                <span className="field">Radius Clause:</span>
+                {offer.radius_clause}
+              </p>
+              <p>
+                <span className="field">Door Times:</span> {offer.door_times}
+              </p>
+              <p>
+                <span className="field">Ages:</span> {offer.age_range}
+              </p>
+              <p>
+                <span className="field">Merch Split:</span> {offer.merch_split}
+              </p>
+            </section>
+            <div className="financials">
+              <h3>Financials</h3>
+              <p>
+                <span className="field">Guarentee:</span> ${offer.guarantee}
+              </p>
+              <p>
+                <span className="field">Total Expenses:</span> $
+                {offer.total_expenses}
+              </p>
+              <p>
+                <span className="field">Gross Potential:</span> $
+                {offer.gross_potential}
+              </p>
+            </div>
+          </div>
+          <p onClick={closeViewOfferModal}>x Close</p>
         </div>
       );
     }
 
     return (
-      <div className="offer-modal">
+      <div className="view-offer-modal">
         <div className="inner-modal">
-          <h1>ViewOfferModal</h1>
+          <h1>{currentVenue.name}</h1>
           {displayOfferInfo}
           <p onClick={closeViewOfferModal}>x Cancel</p>
         </div>
