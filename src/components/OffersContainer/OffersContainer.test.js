baaa38b14393a878d6f831f8aa46a33dc35b7ac4
Gray Smith <gray.smith00@gmail.com>
2018-11-11 14:58:38 -0700

    Update and add tests for ViewOfferModal

--- src/components/OffersContainer/OffersContainer.test.js
+++ src/components/OffersContainer/OffersContainer.test.js
@@ -7,16 +7,19 @@ describe('OffersContainer component', () => {
   let wrapper;
   let mockDate;
   let mockOpenEditOfferModal;
+  let mockOpenViewOfferModal;
 
   beforeEach(() => {
     mockDate = new Date(2019, 11, 10);
     mockOpenEditOfferModal = jest.fn();
+    mockOpenViewOfferModal = jest.fn();
 
     wrapper = shallow(
       <OffersContainer
         date={mockDate}
         offers={mockOffers}
         openEditOfferModal={mockOpenEditOfferModal}
+        openViewOfferModal={mockOpenViewOfferModal}
       />
     );
   });
