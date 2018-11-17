55f0daf609a52f6b1ca3f26060b2a3ebb976b96f
Gray Smith <gray.smith00@gmail.com>
2018-11-17 13:02:02 -0700

    Mock out axios

--- src/Utils/__mocks__/axios.js
+++ src/Utils/__mocks__/axios.js
@@ -1,6 +1,8 @@
 export default {
   get: jest.fn(() => Promise.resolve({ data: {} })),
   post: jest.fn(() => Promise.resolve({ data: {} })),
+  put: jest.fn(() => Promise.resolve({ data: {} })),
+  delete: jest.fn(() => Promise.resolve({ data: {} })),
   defaults: {
     headers: {
       common: {
