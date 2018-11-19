81a4b93e8c454feafc60f94f15811a76d8f040f5
Gray Smith <gray.smith00@gmail.com>
2018-11-18 22:28:16 -0700

    Add market-dash section to Landing page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import SignUp from '../SignUp/SignUp';
 import LogIn from '../LogIn/LogIn';
-import './Landing.css';
 import Splash from '../Splash/Splash';
+import dashImage from './dashImage.jpg';
+import './Landing.css';
 
 class Landing extends Component {
   state = {
@@ -56,7 +57,32 @@ class Landing extends Component {
             <div className="divider-line" />
           </section>
         </section>
-        <section className="market-dashboard" />
+        <section className="market-dash">
+          <div className="market-dash-text">
+            <h2 className="section-title">
+              Organize all your shows in the dashboard.
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos magni
+              illum voluptates maiores eveniet minus eum laboriosam quos quo
+              soluta!
+            </p>
+          </div>
+          <img src={dashImage} alt="dashboard" style={{ height: '250px' }} />
+        </section>
+        <section className="market-browse">
+          <img src={dashImage} alt="dashboard" style={{ height: '250px' }} />
+          <div className="market-dash-text">
+            <h2 className="section-title">
+              Organize all your shows in the dashboard.
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos magni
+              illum voluptates maiores eveniet minus eum laboriosam quos quo
+              soluta!
+            </p>
+          </div>
+        </section>
       </div>
     );
   }
