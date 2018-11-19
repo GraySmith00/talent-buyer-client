284dbbb6ce597ff8dc22df37e2f2c7986c32b785
Gray Smith <gray.smith00@gmail.com>
2018-11-18 17:33:54 -0700

    Adjust ModalButton styledComponent

--- src/components/styledComponents/ModalButton.js
+++ src/components/styledComponents/ModalButton.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 const ModalButton = styled.button`
-  margin-top: 2rem;
   width: 40%;
   padding: 10px 20px;
   border: none;
@@ -10,6 +9,9 @@ const ModalButton = styled.button`
   background-color: #d52f7f;
   color: #fff;
   cursor: pointer;
+  -webkit-box-shadow: 10px 10px 57px -16px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 10px 10px 57px -16px rgba(0, 0, 0, 0.75);
+  box-shadow: 10px 10px 57px -16px rgba(0, 0, 0, 0.75);
 `;
 
 export default ModalButton;
